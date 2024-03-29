2715c0367ab46f8b9c250358795d3997b2bbd417
facpbmdev <facbpmcontato@gmail.com>
2024-03-29 19:58:22 -0300

    font-family-config

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -6,7 +6,7 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Changa One', sans-serif;
+    font-family: 'Changa One';
     font-family: 'Roboto', sans-serif;
 }
 
